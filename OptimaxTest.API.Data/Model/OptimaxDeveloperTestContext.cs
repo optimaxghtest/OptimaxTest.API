74a32dd8f726015f842b990b10bb885c53497472
adam-strain-dev <spotas33@gmail.com>
2022-07-05 18:09:25 +0100

    Updated connection string

--- OptimaxTest.API.Data/Model/OptimaxDeveloperTestContext.cs
+++ OptimaxTest.API.Data/Model/OptimaxDeveloperTestContext.cs
@@ -22,13 +22,7 @@ namespace OptimaxTest.API.Data.Model
         public virtual DbSet<UserRolePermission> UserRolePermissions { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=OPTLONTESTSQL02;Database=OptimaxDeveloperTest;Trusted_Connection=True;");
-            }
-        }
+        {}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
